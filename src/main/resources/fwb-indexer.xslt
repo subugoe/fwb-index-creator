447149cd220e3f4877bf9738c2b83bd5a1b69395
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-02-19 13:01:49 +0000

    Add first trivial test case

--- src/main/resources/fwb-indexer.xslt
+++ src/main/resources/fwb-indexer.xslt
@@ -1,7 +1,8 @@
 <?xml version="1.0" encoding="utf-8"?>
 <xsl:stylesheet version="2.0"
-    xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:fwb="http://www.sub.uni-goettingen.de/FWB"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:tei="http://www.tei-c.org/ns/1.0"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    exclude-result-prefixes="tei xsi">
 
     <xsl:output method="xml" indent="yes" />
 
@@ -9,5 +10,10 @@
         <xsl:apply-templates />
     </xsl:template>
 
+    <xsl:template match="tei:title">
+        <found>
+            <xsl:value-of select="text()"/>
+        </found>
+    </xsl:template>
 
 </xsl:stylesheet>
--- src/test/java/sub/fwb/IndexerTest.java
+++ src/test/java/sub/fwb/IndexerTest.java
@@ -2,14 +2,24 @@ package sub.fwb;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.OutputStream;
+
+import javax.xml.transform.stream.StreamSource;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import net.sf.saxon.s9api.*;
 
 public class IndexerTest {
+	
+	private OutputStream outputBaos;
 
 	@Before
 	public void setUp() throws Exception {
+		outputBaos = new ByteArrayOutputStream();
 	}
 
 	@After
@@ -17,8 +27,21 @@ public class IndexerTest {
 	}
 
 	@Test
-	public void test() {
-		
+	public void test() throws SaxonApiException {
+        Processor proc = new Processor(false);
+        XsltCompiler comp = proc.newXsltCompiler();
+        XsltExecutable exp = comp.compile(new StreamSource(new File("src/main/resources/fwb-indexer.xslt")));
+        XdmNode source = proc.newDocumentBuilder().build(new StreamSource(new File("src/test/resources/test.xml")));
+        Serializer out = proc.newSerializer();
+        out.setOutputProperty(Serializer.Property.METHOD, "xml");
+        out.setOutputProperty(Serializer.Property.INDENT, "yes");
+        out.setOutputStream(outputBaos);
+        XsltTransformer trans = exp.load();
+        trans.setInitialContextNode(source);
+        trans.setDestination(out);
+        trans.transform();
+
+        System.out.println(outputBaos.toString());
 	}
 
 }
