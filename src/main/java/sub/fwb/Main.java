99b4d3577770ef016a53ad50d443c3855473a7de
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-03-07 11:24:38 +0000

    Enhance main class to work on whole directories

--- src/main/java/sub/fwb/Main.java
+++ src/main/java/sub/fwb/Main.java
@@ -1,30 +1,50 @@
 package sub.fwb;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		
+		
 		if (args.length != 2) {
 			System.out.println("Syntax: java -jar indexer.jar <input-dir> <output-dir>");
 		} else {
 			File inputDir = new File(args[0]);
 			File outputDir = new File(args[1]);
 			
-			TreeSet<File> allFiles = new TreeSet<File>();
-			fillWithFiles(allFiles, inputDir);
-			
 			InputStream xsltStream = Main.class.getResourceAsStream("/fwb-indexer.xslt");
 			Xslt xslt = new Xslt(xsltStream);
 			
-			//for (File f : )
+			
+			long before = new Date().getTime();
+
+			ArrayList<File> allFiles = new ArrayList<File>();
+			fillWithFiles(allFiles, inputDir);
+			Collections.sort(allFiles);
+			
+			int i = 0;
+			for (File currentFile : allFiles) {
+				File previousFile = i>0 ? allFiles.get(i-1) : null;
+				File nextFile = i<allFiles.size()-1 ? allFiles.get(i+1) : null;
+				xslt.setParameter("currentArticleId", "" + (i+1));
+				xslt.transform(currentFile.getAbsolutePath(), new FileOutputStream(new File(outputDir, ""+(i+1)+".xml")));
+				i++;
+			}
+			
+			long after = new Date().getTime();
+			System.out.println("Took " + (after - before) + " milliseconds");
+			
 		}
 
 	}
 	
-	private static void fillWithFiles(TreeSet<File> allFiles, File currentDir) {
+	private static void fillWithFiles(ArrayList<File> allFiles, File currentDir) {
 		File[] currentDirChildren = currentDir.listFiles();
 		for (File child : currentDirChildren) {
 			if (child.isFile() && child.getName().endsWith("xml")) {
