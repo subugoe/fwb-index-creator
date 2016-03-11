bcb5e6c79d9328e61b247a01c92684770220d05b
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-03-11 09:37:43 +0100

    Create target directory if it does not exist
    This adds a routine checking if the target directory exists and - if
    not - creates it.

--- src/main/java/sub/fwb/Main.java
+++ src/main/java/sub/fwb/Main.java
@@ -10,24 +10,40 @@ import java.util.Date;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		
-		
+
+
 		if (args.length != 2) {
 			System.out.println("Syntax: java -jar indexer.jar <input-dir> <output-dir>");
 		} else {
 			File inputDir = new File(args[0]);
 			File outputDir = new File(args[1]);
-			
+
+			if (!outputDir.exists()) {
+				System.out.println("Creating directory: " + outputDir);
+				boolean result = false;
+
+				try{
+					outputDir.mkdir();
+					result = true;
+				}
+				catch(SecurityException se){
+					//handle it
+				}
+				if(result) {
+					System.out.println(outputDir + " created");
+				}
+			}
+
 			InputStream xsltStream = Main.class.getResourceAsStream("/fwb-indexer.xslt");
 			Xslt xslt = new Xslt(xsltStream);
-			
-			
+
+
 			long before = new Date().getTime();
 
 			ArrayList<File> allFiles = new ArrayList<File>();
 			fillWithFiles(allFiles, inputDir);
 			Collections.sort(allFiles);
-			
+
 			int i = 0;
 			for (File currentFile : allFiles) {
 				File previousFile = i>0 ? allFiles.get(i-1) : null;
@@ -36,14 +52,14 @@ public class Main {
 				xslt.transform(currentFile.getAbsolutePath(), new FileOutputStream(new File(outputDir, ""+(i+1)+".xml")));
 				i++;
 			}
-			
+
 			long after = new Date().getTime();
 			System.out.println("Took " + (after - before) + " milliseconds");
-			
+
 		}
 
 	}
-	
+
 	private static void fillWithFiles(ArrayList<File> allFiles, File currentDir) {
 		File[] currentDirChildren = currentDir.listFiles();
 		for (File child : currentDirChildren) {
--- src/test/java/sub/fwb/IndexerTest.java
+++ src/test/java/sub/fwb/IndexerTest.java
@@ -11,15 +11,15 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class IndexerTest {
-	
+
 	private OutputStream outputBaos;
 	private static Xslt xslt;
-	
+
 	@BeforeClass
 	public static void beforeAllTests() throws Exception {
 		xslt = new Xslt("src/main/resources/fwb-indexer.xslt");
 	}
-	
+
 	@Before
 	public void beforeEachTest() throws Exception {
 		outputBaos = new ByteArrayOutputStream();
@@ -27,136 +27,136 @@ public class IndexerTest {
 
 	@After
 	public void afterEachTest() {
-        //System.out.println(outputBaos.toString());	
+		//System.out.println(outputBaos.toString());
 	}
-	
+
 	@Test
 	public void shouldTransformPrintedSource() throws Exception {
 		xslt.transform("src/test/resources/printedSource.xml", outputBaos);
-        String result = outputBaos.toString();
+		String result = outputBaos.toString();
 
-        assertXpathEvaluatesTo("artikel", "//field[@name='type']", result);
-        assertXpathEvaluatesTo("Some printed source", "//field[@name='printed_source']", result);
-        assertXpathEvaluatesTo("08", "//field[@name='volume']", result);
-        assertXpathEvaluatesTo("1", "//field[@name='col']", result);
+		assertXpathEvaluatesTo("artikel", "//field[@name='type']", result);
+		assertXpathEvaluatesTo("Some printed source", "//field[@name='printed_source']", result);
+		assertXpathEvaluatesTo("08", "//field[@name='volume']", result);
+		assertXpathEvaluatesTo("1", "//field[@name='col']", result);
 	}
-	
+
 	@Test
 	public void shouldFindArticleEntries() throws Exception {
 		xslt.transform("src/test/resources/articleEntries.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("some.id", "//field[@name='internal_id']", result);
-        assertXpathEvaluatesTo("test_lemma,", "//field[@name='lemma']", result);
-        assertXpathEvaluatesTo("test_lemma", "//field[@name='lemma_normalized']", result);
-        assertXpathEvaluatesTo("Pron.", "//field[@name='type_of_word']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("some.id", "//field[@name='internal_id']", result);
+		assertXpathEvaluatesTo("test_lemma,", "//field[@name='lemma']", result);
+		assertXpathEvaluatesTo("test_lemma", "//field[@name='lemma_normalized']", result);
+		assertXpathEvaluatesTo("Pron.", "//field[@name='type_of_word']", result);
 	}
-	
+
 	@Test
 	public void shouldTransformLemmaWithoutComma() throws Exception {
 		xslt.transform("src/test/resources/lemmaWithoutComma.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("test_lemma", "//field[@name='lemma']", result);
-        assertXpathEvaluatesTo("test_lemma", "//field[@name='lemma_normalized']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("test_lemma", "//field[@name='lemma']", result);
+		assertXpathEvaluatesTo("test_lemma", "//field[@name='lemma_normalized']", result);
 	}
-	
+
 	@Test
 	public void shouldNormalizeLemmaWithSpace() throws Exception {
 		xslt.transform("src/test/resources/lemmaWithSpace.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("test_lemma,", "//field[@name='lemma']", result);
-        assertXpathEvaluatesTo("test_lemma", "//field[@name='lemma_normalized']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("test_lemma,", "//field[@name='lemma']", result);
+		assertXpathEvaluatesTo("test_lemma", "//field[@name='lemma_normalized']", result);
 	}
-	
+
 	@Test
 	public void shouldTransformOneVariant() throws Exception {
 		xslt.transform("src/test/resources/oneNotationVariant.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("1", "count(//field[@name='notation_variant'])", result);
-        assertXpathEvaluatesTo("tesst", "//field[@name='notation_variant']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("1", "count(//field[@name='notation_variant'])", result);
+		assertXpathEvaluatesTo("tesst", "//field[@name='notation_variant']", result);
 	}
-	
+
 	@Test
 	public void shouldTransformTwoVariants() throws Exception {
 		xslt.transform("src/test/resources/twoNotationVariants.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("2", "count(//field[@name='notation_variant'])", result);
-        assertXpathEvaluatesTo("tesst", "//field[@name='notation_variant'][1]", result);
-        assertXpathEvaluatesTo("tesdt", "//field[@name='notation_variant'][2]", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("2", "count(//field[@name='notation_variant'])", result);
+		assertXpathEvaluatesTo("tesst", "//field[@name='notation_variant'][1]", result);
+		assertXpathEvaluatesTo("tesdt", "//field[@name='notation_variant'][2]", result);
 	}
-	
+
 	@Test
 	public void shouldRecognizeRefArticle() throws Exception {
 		xslt.transform("src/test/resources/articleRef.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("true", "//field[@name='is_reference']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("true", "//field[@name='is_reference']", result);
 	}
-	
+
 	@Test
 	public void shouldRecognizeNormalArticle() throws Exception {
 		xslt.transform("src/test/resources/articleNotRef.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("false", "//field[@name='is_reference']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("false", "//field[@name='is_reference']", result);
 	}
-	
+
 	@Test
 	public void shouldSetArticleId() throws Exception {
 		xslt.setParameter("currentArticleId", "123");
 		xslt.transform("src/test/resources/articleId.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("123", "//field[@name='id']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("123", "//field[@name='id']", result);
 	}
-	
+
 	@Test
 	public void shouldSetSenseId() throws Exception {
 		xslt.setParameter("currentArticleId", "123");
 		xslt.transform("src/test/resources/senseId.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("123_1", "//doc[2]/field[@name='id']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("123_1", "//doc[2]/field[@name='id']", result);
 	}
-	
+
 	@Test
 	public void shouldTransformOneSense() throws Exception {
 		xslt.transform("src/test/resources/oneSense.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("1", "count(//field[text()='bedeutung'])", result);
-        assertXpathExists("//field[text()='Definition.']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("1", "count(//field[text()='bedeutung'])", result);
+		assertXpathExists("//field[text()='Definition.']", result);
 	}
-	
+
 	@Test
 	public void shouldTransformTwoSenses() throws Exception {
 		xslt.transform("src/test/resources/twoSenses.xml", outputBaos);
-        String result = outputBaos.toString();
-		
-        assertXpathEvaluatesTo("2", "count(//field[text()='bedeutung'])", result);
-        assertXpathExists("//field[text()='Definition one.']", result);
-        assertXpathExists("//field[text()='Definition two.']", result);
+		String result = outputBaos.toString();
+
+		assertXpathEvaluatesTo("2", "count(//field[text()='bedeutung'])", result);
+		assertXpathExists("//field[text()='Definition one.']", result);
+		assertXpathExists("//field[text()='Definition two.']", result);
 	}
-	
+
 	@Test
 	public void should() throws Exception {
 		//xslt.transform("/home/dennis/temp/i/i/in.in.s.7pr.xml", outputBaos);
- 		
- 	}
+
+	}
 	@Test
 	public void shouldRef() throws Exception {
 		//xslt.transform("/home/dennis/temp/i/i/it.it.s.9ref.xml", outputBaos);
- 		
- 	}
+
+	}
 	@Test
 	public void shouldArzeneien() throws Exception {
 		//xslt.setParameter("currentArticleId", "123");
 		//xslt.transform("/home/dennis/Downloads/arzneien.arzneien.s.3v.xml", outputBaos);
- 		
- 	}
+
+	}
 
 }
