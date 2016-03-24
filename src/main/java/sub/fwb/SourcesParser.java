03c34bf485a91791f665fe49610ed3e4f4fb02e7
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-03-24 11:34:11 +0000

    Start to create XML out of the Excel file

--- src/main/java/sub/fwb/SourcesParser.java
+++ src/main/java/sub/fwb/SourcesParser.java
@@ -14,36 +14,70 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class SourcesParser {
+	
+	private String[] headers = {"sort", "sigle", "kraftliste", "", "", "", "", "", "", "", "pdf", "epdf", "online", "eonline", "permalink", "biblio", "citing", "syptom"};
 
 	public void bla() throws IOException {
 		FileInputStream file = new FileInputStream(new File("/home/dennis/temp/FWB-Quellenliste2.xlsx"));
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("<sheet>\n");
 
 		XSSFWorkbook workbook = new XSSFWorkbook(file);
 
 		XSSFSheet sheet = workbook.getSheetAt(0);
 
-		for (int i = 1; i < 1321; i++) {
-
-			Row r = sheet.getRow(i);
-
-			Cell c = r.getCell(2);
-			String wholeCell = c.getStringCellValue();
-
-			String foundName = extractUsingRegex("\\$c(.*?)#", wholeCell).get(0);
-			System.out.println();
-			// System.out.println(foundName);
-
-			Cell biblioCell = r.getCell(15);
-			String biblio = biblioCell.getStringCellValue();
-			String hrsg = extractUsingRegex("[hH]rsg\\. v\\.\\s*(.*?\\w\\w)\\.", biblio).get(0);
-			System.out.println(i + 1 + ": " + hrsg);
-
-			List<String> years = extractUsingRegex("(\\w*\\s1\\d\\d\\d)", biblio);
-			for (String year : years) {
-				System.out.println(i + 1 + ": " + year);
+		for (int i = 1; i <= 49; i++) {
+			
+			buffer.append("<entry>\n");
+			Row row = sheet.getRow(i);
+			for (int j = 0; j < headers.length; j++) {
+				Cell cell = row.getCell(j);
+				if (cell != null) {
+					buffer.append("<" + headers[j] + ">");
+					if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+						buffer.append(cell.getStringCellValue());
+					} else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+						buffer.append(cell.getNumericCellValue());
+					}
+					buffer.append("</" + headers[j] + ">\n");
+				}
+				if (j == 2) {
+					j += 7;
+				}
 			}
+			
+			buffer.append("</entry>\n");
+			
+			
+			
+//			Row r = sheet.getRow(i);
+//
+//			Cell c = r.getCell(2);
+//			String wholeCell = "";
+//			if (c != null)
+//				wholeCell = c.getStringCellValue();
+//
+//			String foundName = extractUsingRegex("\\$c(.*?)#", wholeCell).get(0);
+//			//System.out.println();
+//			// System.out.println(foundName);
+//
+//			Cell biblioCell = r.getCell(15);
+//			String biblio = "";
+//			if (biblioCell != null)
+//				biblio = biblioCell.getStringCellValue();
+//			
+//			String hrsg = extractUsingRegex("[hH]rsg\\. v\\.\\s*(.*?\\w\\w)\\.", biblio).get(0);
+//			//System.out.println(i + 1 + ": " + hrsg);
+//
+//			List<String> years = extractUsingRegex("(\\w*\\s1\\d\\d\\d)", biblio);
+//			for (String year : years) {
+//				//System.out.println(i + 1 + ": " + year);
+//			}
 		}
 		workbook.close();
+		
+		buffer.append("</sheet>");
+		System.out.println(buffer);
 	}
 
 	private List<String> extractUsingRegex(String regex, String s) {
