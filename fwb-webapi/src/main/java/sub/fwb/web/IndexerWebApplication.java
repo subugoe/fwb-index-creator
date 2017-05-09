d06ae8eda316d86858f85e8f8c2a7fe76277a010
Dennis Neumann <neumann@sub.uni-goettingen.de>
2017-05-09 14:27:10 +0000

    Add module for web frontend

---
+++ fwb-webapi/src/main/java/sub/fwb/web/IndexerWebApplication.java
@@ -0,0 +1,12 @@
+package sub.fwb.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexerWebApplication.class, args);
+	}
+}
---
+++ fwb-webapi/src/main/java/sub/fwb/web/MainController.java
@@ -0,0 +1,31 @@
+package sub.fwb.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/test2")
+	@ResponseBody
+	public ResponseEntity<?> getTest() {
+		return ResponseEntity.ok().body("some test");
+	}
+
+	@RequestMapping(value="/")
+	public String index(Model model) {
+		model.addAttribute("test", "blablub");
+		return "index";
+	}
+
+	@RequestMapping(value="/importstaging")
+	public String importstaging(@RequestParam("mailaddress") String mail, Model model) {
+		model.addAttribute("processingMessage", "In Kürze wird ein Bericht verschickt an: " + mail);
+		return "message";
+	}
+}
